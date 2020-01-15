ea759a7128e7a403a130e86fd63f216eea35114b
Oleksandr Pishcheiyko <pishcheiyko@gmail.com>
2020-01-15 11:53:49 +0200

    fix

--- src/routes.php
+++ src/routes.php
@@ -1,33 +1,31 @@
 <?php
 
-Route::middleware(config('saml2_settings.routesMiddleware'))
-    ->prefix(config('saml2_settings.routesPrefix').'/')->group(function() {
-        Route::prefix('{idpName}')->group(function() {
-            $saml2_controller = config('saml2_settings.saml2_controller', 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller');
+Route::group(['middleware' => config('saml2_settings.routesMiddleware')], function() {
+    Route::group(['prefix' => '{idpName}'], function(){
+        $saml2_controller = config('saml2_settings.saml2_controller', 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller');
+        Route::get('/logout', array(
+            'as' => 'saml2_logout',
+            'uses' => $saml2_controller.'@logout',
+        ));
 
-            Route::get('/logout', array(
-                'as' => 'saml2_logout',
-                'uses' => $saml2_controller.'@logout',
-            ));
+        Route::get('/login', array(
+            'as' => 'saml2_login',
+            'uses' => $saml2_controller.'@login',
+        ));
 
-            Route::get('/login', array(
-                'as' => 'saml2_login',
-                'uses' => $saml2_controller.'@login',
-            ));
+        Route::get('/metadata', array(
+            'as' => 'saml2_metadata',
+            'uses' => $saml2_controller.'@metadata',
+        ));
 
-            Route::get('/metadata', array(
-                'as' => 'saml2_metadata',
-                'uses' => $saml2_controller.'@metadata',
-            ));
+        Route::post('/acs', array(
+            'as' => 'saml2_acs',
+            'uses' => $saml2_controller.'@acs',
+        ));
 
-            Route::post('/acs', array(
-                'as' => 'saml2_acs',
-                'uses' => $saml2_controller.'@acs',
-            ));
-
-            Route::get('/sls', array(
-                'as' => 'saml2_sls',
-                'uses' => $saml2_controller.'@sls',
-            ));
-        });
+        Route::get('/sls', array(
+            'as' => 'saml2_sls',
+            'uses' => $saml2_controller.'@sls',
+        ));
     });
+});
